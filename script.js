5ed6fef854f2d32ad01ced3daf0cbeb0429403fe
leon-Sakura-web <leonray0603@gmail.com>
2026-06-04 16:51:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("scrollBtn").addEventListener("click", () => {
+  document.querySelector(".works").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+cards.forEach(card => observer.observe(card));
